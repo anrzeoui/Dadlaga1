79e4d380b14449383588b18a5e21d9fa9f099a45
anrzeoui <anar.urnukhbayar@gmail.com>
2025-06-02 15:08:48 +0800

    Person

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Person p = new Person();
+        p.name = "Anar";
+        p.age = 20;
+        p.greet();
+    }
+}
---
+++ Person.java
@@ -0,0 +1,8 @@
+public class Person {
+    String name;
+    int age;
+
+    public void greet() {
+        System.out.println("Hi! I'm " + name + ", and I'm " + age + " years old.");
+    }
+}
